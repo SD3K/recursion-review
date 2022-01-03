1bb05b7ab26cf67bc8ea8e38bf55a5528ae51d97
Skipper Harris <skipdawg3000@gmail.com>
2022-01-03 14:50:17 -0800

    passing stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,60 @@
 
 // but you don't so you're going to write it from scratch:
 
+//contains object brackets in string
+//double quotes for key value
+//functions/undefined = null
+//input: object
+//output: string
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  var returnValue = '';
+  if (typeof obj === 'undefined' || typeof obj === 'function' || obj === null) {
+    returnValue += 'null';
+  }
+  if (typeof obj === 'string') {
+    returnValue += '\"' + obj + '\"';
+  }
+  if (typeof obj === 'number') {
+    returnValue += obj;
+  }
+  if (typeof obj === 'boolean') {
+    if (obj) {
+      returnValue += 'true';
+    } else {
+      returnValue += 'false';
+    }
+  }
+  if (Array.isArray(obj)) {
+    returnValue += '[';
+    for (var i = 0; i < obj.length; i++) {
+      returnValue += stringifyJSON(obj[i]);
+      if (i < obj.length - 1) {
+        returnValue += ',';
+      }
+    }
+    returnValue += ']';
+  } else if (typeof obj === 'object' && obj !== null) {
+    if (Object.keys(obj).length === 0) {
+      returnValue += '{}';
+    } else {
+      returnValue += '{';
+      for (var key in obj) {
+        if (typeof obj[key] === 'undefined' || typeof obj[key] === 'function') {
+          continue;
+        }
+        returnValue += stringifyJSON(key);
+        returnValue += ':';
+        returnValue += stringifyJSON(obj[key]);
+        returnValue += ',';
+      }
+      returnValue = returnValue.slice(0, returnValue.length - 1);
+      returnValue += '}';
+    }
+  }
+  if (returnValue === '}') {
+    returnValue = '{}';
+  }
+  return returnValue;
 };
