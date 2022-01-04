f955aa567509558d69f03142020f268cd5538ba4
Skipper Harris <skipdawg3000@gmail.com>
2022-01-03 17:56:22 -0800

    passing getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,39 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+
+//input = string
+//output = "array"
+
+
+var getElementsByClassName = function(className) {
+  var returnValue = [];
+  var body = document.body;
+
+  // take 2
+  var insideFunction = function (element) {
+    if (element.classList !== undefined && element.classList.contains(className)) {
+      returnValue.push(element);
+    }
+    for (var i = 0; i < element.childNodes.length; i++) {
+      insideFunction(element.childNodes[i]);
+    }
+  };
+  insideFunction(body);
+  return returnValue;
 };
+
+// iterate through document body to see if element.classList contains search parameter
+// for (var i = 0; i < body.childNodes.length; i++) {
+//   var current = body.childNodes[i];
+//   // if current element has child node then run function recursively on child node
+//   if (current.childNodes) {
+//     current.forEach(getElementsByClassName(className));
+//   }
+//   if (current.classList && current.classList.contains(className)) {
+//     returnValue.push(current);
+//   }
+// }
+
+// return returnValue;
